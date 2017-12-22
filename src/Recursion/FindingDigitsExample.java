01b05022e6b7814064789e73c723b78ae2387511
damlakoksal <damlakoksal@outlook.com>
2017-12-22 14:29:51 +0300

    Changed location of digit variable. There is only problem with input zero.

--- src/Recursion/FindingDigitsExample.java
+++ src/Recursion/FindingDigitsExample.java
@@ -1,26 +1,30 @@
 package Recursion;
 
 public class FindingDigitsExample {
+	
+	public static int digit=0; //Digit has to be static and at the outside of the findDigit method.
+	
+	public static int findDigit(int number) { //Recursive functions can not have void.	
 
-	public static int findDigit(int number) { //Recursive functions can not have void.
-		
-		int digit=0;
-		
 		if(number!= 0) {
 			digit++;
 			return findDigit(number/10);
 		}
 		
-		else {		
+		else if(number == 0) {		
 			return digit;
 		}
+		
+		else //for invalid conditions it returns 1.
+			return 1; 
+		
 	}
 	
 	public static void main(String[]args) {
 		
-		int a=findDigit(124); 
+		int a=findDigit(50);
 		System.out.println(a);
-		
+			
 	}
 }
-//This version does not working. Trying to find out the solution.
+//This version is working but there is a problem with zero.
