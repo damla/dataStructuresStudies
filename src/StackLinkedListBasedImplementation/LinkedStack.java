55097e7e47915f72792dc941f1a19273baa8360e
damlakoksal <damlakoksal@outlook.com>
2017-12-24 23:44:48 +0300

    Added linked list based implementation in stack.

--- src/StackLinkedListBasedImplementation/LinkedStack.java
+++ src/StackLinkedListBasedImplementation/LinkedStack.java
@@ -1,5 +1,102 @@
 package StackLinkedListBasedImplementation;
 
 public class LinkedStack {
+/*   --Notes About Linked List Based Stack--
+ * 
+ * No need for fixed number of elements.
+ * Linked list can dynamically organize data.
+ * In a linked representation, top is a node for the top element in stack.
+ */
+	
+	private Node top;
+	
+	public LinkedStack() {
+// Initializes the stack and sets top to null.
+		
+		top=null;
+		
+	}
+	
+	public boolean isEmpty() {
+// Checks if the list is empty or not.
+// The stack only will be full if we run out of memory.
+		
+		return(top==null); 
+	}
+	
+	public void initializeStack() {
+		
+		top=null; 
+		
+	}
+	
+	public void push(int newElement) {
+		
+		Node newNode=new Node(newElement); //link is null
+		newNode.setLink(top); 
+		top=newNode;
+		
+	}
+	
+	public int top() {
+// Demonstrates the top element.
+		
+		assert(top==null); //if the statement is true, it will terminate.
+		
+		return top.getInfo(); //gets the data of top element.
+	}
+	
+	public boolean pop() {
+// Check if the list is empty or not, then delete the first element by changing tops link.		
+		
+		if(isEmpty()) {
+			return false;
+		}
+		else {
+		top=top.getLink(); //Setting the link of top to the next element so this will delete the top element.
+		return true;
+		}
+	}
+	
+	public void outputLinkedStack() {
+		
+		Node current=top; //current node will go around the list from top to the last element.
+		
+		while(current!=null) {
+			
+			System.out.println(current.getInfo());
+			current=current.getLink();
+			
+		}
+	}
+	
+	public void copyStack(LinkedStack otherStack) {
+// The stack taken as an argument is assigned to this stack.
+		
+		top=otherStack.top; 
+		
+	}
+	
+	public boolean equals(LinkedStack otherStack) {
+// Checks whether the top of the stack taken as the argument is equal to the top of the stack calling the method.		
+	
+		if(top==otherStack.top) {
+			return true;
+		}
+		else
+			return false;
+}
+	
+	public int size() {
+// Checks the size of list.		
+		int count=0;
+		Node current=top;
+		
+		while(current!=null) {
+			count++;
+			current=current.getLink();
+		}
+		return count;
 
+	}		
 }
--- src/StackLinkedListBasedImplementation/Node.java
+++ src/StackLinkedListBasedImplementation/Node.java
@@ -2,4 +2,46 @@ package StackLinkedListBasedImplementation;
 
 public class Node {
 
+	private int info;
+	private Node link;
+	
+// Getters and Setters.	
+	public int getInfo() {
+		return info;
+	}
+
+	public void setInfo(int info) {
+		this.info = info;
+	}
+
+	public Node getLink() {
+		return link;
+	}
+	
+	public void setLink(Node link) {
+		this.link=link;
+	}
+	
+	public Node() {
+		
+		link=null;
+		info=0;
+		
+	}
+	
+	public Node(int newInfo) {
+		
+		setInfo(newInfo); //creating node with info.
+		link=null; //link is null so we are adding the node to the last.
+		
+	}
+	
+	public Node(int newInfo, Node linkValue) {
+		
+		setInfo(newInfo);
+		setLink(linkValue); //also setting the link.
+	}
+	
+	
+	
 }
