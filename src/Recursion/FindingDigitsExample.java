033fe0945166351da408a24684393bb80fc499df
damlakoksal <damlakoksal@outlook.com>
2018-01-01 20:15:09 +0300

    Problem with the zero fixed by adding conditions in main method.

--- src/Recursion/FindingDigitsExample.java
+++ src/Recursion/FindingDigitsExample.java
@@ -1,30 +1,40 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class FindingDigitsExample {
 	
 	public static int digit=0; //Digit has to be static and at the outside of the findDigit method.
 	
 	public static int findDigit(int number) { //Recursive functions can not have void.	
 
-		if(number!= 0) {
-			digit++;
-			return findDigit(number/10);
+			if(number!= 0) {
+				digit++;
+				return findDigit(number/10);
+			}
+		
+			else if(number == 0) {		
+				return digit;
+			}
+		
+			else //for invalid conditions it returns 1.
+				return 1; 
 		}
-		
-		else if(number == 0) {		
-			return digit;
-		}
-		
-		else //for invalid conditions it returns 1.
-			return 1; 
-		
-	}
 	
 	public static void main(String[]args) {
 		
-		int a=findDigit(50);
-		System.out.println(a);
-			
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int a=sc.nextInt();
+		
+		if(a==0) {
+			System.out.println(1);
+		}
+		
+		else {
+			int b=findDigit(a);
+			System.out.print(b);
+		}
+		sc.close();
 	}
 }
-//This version is working but there is a problem with zero.
