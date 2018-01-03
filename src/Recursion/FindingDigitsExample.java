839d6f7967f7e8190d0ccc57a1f94293dd00c7b7
damlakoksal <damlakoksal@outlook.com>
2018-01-04 00:09:19 +0300

    Fixed the problem with zero. Program works successfully.

--- src/Recursion/FindingDigitsExample.java
+++ src/Recursion/FindingDigitsExample.java
@@ -1,40 +1,22 @@
 package Recursion;
 
-import java.util.Scanner;
-
 public class FindingDigitsExample {
 	
-	public static int digit=0; //Digit has to be static and at the outside of the findDigit method.
+	public static int digit=1; //Digit has to be static and at the outside of the findDigit method. 
 	
-	public static int findDigit(int number) { //Recursive functions can not have void.	
-
-			if(number!= 0) {
-				digit++;
-				return findDigit(number/10);
-			}
-		
-			else if(number == 0) {		
-				return digit;
-			}
-		
-			else //for invalid conditions it returns 1.
-				return 1; 
-		}
-	
-	public static void main(String[]args) {
-		
-		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter the number: ");
-		int a=sc.nextInt();
-		
-		if(a==0) {
-			System.out.println(1);
-		}
+	public static int findDigit(int number) { //Recursive functions can not have void.
+			
+		if(number<10 && number > -10) //Base case for one digit numbers.
+			return digit;
 		
 		else {
-			int b=findDigit(a);
-			System.out.print(b);
+			digit++;
+			return findDigit(number/10); //Divides the number while increasing digit until it becomes zero. When number is zero, it will return digit.
 		}
-		sc.close();
+		
+	}
+	public static void main(String[]args) {	
+		
+		System.out.println(findDigit(100));
 	}
 }
